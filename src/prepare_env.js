be3021d71e87ad10f39a94a81cb2e93540065881
Douglas Lara <douglasdelaras@gmail.com>
2023-07-05 11:05:34 -0300

    Update script

--- src/prepare_env.js
+++ src/prepare_env.js
@@ -1,7 +1,7 @@
 if (process.env.CONFIG != undefined) {
-  console.log('Change config.json by the environment variable');
+  console.log('Change config.ts by the environment variable');
   var fs = require('fs');
-  var filePath = './dist/config.json';
+  var filePath = './dist/config.ts';
 
   fs.writeFile(filePath, process.env.CONFIG, function (err) {
     if (err) return console.log(err);
